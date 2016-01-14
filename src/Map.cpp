50419d8f7b726ecacdc177c9da71fec88dd589ae
Ryan Weng <ryan4559@gmail.com>
2016-01-14 22:00:37 +0800

    fix logical problem

--- src/Map.cpp
+++ src/Map.cpp
@@ -13,7 +13,7 @@ bool Land::put(Plant & plant)
 
 Map::Map(int land_num)
 {
-	if(land_num > 0 && land_num <= max_land_num)
+	if(!(land_num > 0 && land_num <= max_land_num))
 		throw "Number of Land should less than " + max_land_num;
 
 	for(int i = 0 ; i < land_num ; ++i)
